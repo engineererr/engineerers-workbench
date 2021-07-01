355211d12bf35e5d0e694185742136b4903bc98d
Kai Boschung <kbo@expertsinside.com>
2021-07-01 14:10:20 +0200

    added graph scripts

---
+++ Graph API/Get-StaleUsersReport.ps1
@@ -0,0 +1,18 @@
+<#
+Source: https://docs.microsoft.com/en-us/graph/api/user-get?view=graph-rest-beta&tabs=http
+#>
+Install-Module -Name MSAL.PS -RequiredVersion 4.2.1.3
+Import-Module MSAL.PS
+$tenantID = ""
+$appId = ""
+$appSecret = ""
+
+$ConfidentialClientOptions = New-Object Microsoft.Identity.Client.ConfidentialClientApplicationOptions -Property @{ ClientId = $appId; TenantId = $tenantID; ClientSecret = $appSecret }
+$ConfidentialClient = $ConfidentialClientOptions | New-MsalClientApplication
+$ConfidentialClient.AcquireTokenSilent(
+$tokenObj = Get-MsalToken -Scope 'https://graph.microsoft.com/.default' -ConfidentialClientApplication $ConfidentialClient
+$tokenObj = Get-MsalToken -Scope 'https://graph.microsoft.com/.default' -ClientId $appId -ClientSecret (ConvertTo-SecureString -String $appSecret -AsPlainText -Force) -TenantId $tenantID
+$apiUrl = "https://graph.microsoft.com/beta/users?filter=signInActivity/lastSignInDateTime le 2021-06-21T00:00:00Z&`$select=userPrincipalName,displayName,mail,signInActivity"
+$res = Invoke-RestMethod -Headers @{Authorization = "Bearer $($tokenObj.AccessToken)"} -Uri $apiUrl -Method Get
+
+$res.value | select userPrincipalName, displayName, mail, @{L="LastSignInDateTime";E={$_.signInActivity.lastSignInDateTime}} | Sort-Object -Property LastSignInDateTime
